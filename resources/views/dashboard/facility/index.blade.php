dfe8839aebfdc1cd465e573bb7e5f34ad0771677
Ujun Junaedi <jujunjuna189@gmail.com>
2025-11-27 07:09:51 +0700

    Update

--- resources/views/dashboard/facility/index.blade.php
+++ resources/views/dashboard/facility/index.blade.php
@@ -167,8 +167,18 @@
 
         let freeGuest = "";
         if(item.is_free_for_guest == 1){
-            freeGuest = `<div class="bg-[#EAC580] flex items-center gap-1 rounded-full px-2 py-1 mb-1">
-                <span class="text-[10px]"><strong>FREE</strong> untuk tamu menginap</span>
+            freeGuest = `
+            <div class="flex justify-between">
+                <div class="bg-[#EAC580] flex items-center gap-1 rounded-full px-2 py-1 mb-1">
+                    <span class="text-[10px]"><strong>FREE</strong> untuk tamu menginap</span>
+                </div>
+                <div class="bg-[#F2F4F7] flex gap-1 rounded-full px-2 py-1 items-center justify-end">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="currentColor" class="text-yellow-500">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z" />
+                    </svg>
+                    <span class="text-[12px]">${item.rating}</span>
+                </div>
             </div>`;
         }
 
