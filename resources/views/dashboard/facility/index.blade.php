61cbfa30f4d9f5d5c08075cbddbc377361e06438
maulana-kurniawan <117083636+maulana-kurniawan@users.noreply.github.com>
2025-11-30 08:35:04 +0700

    bold font for rating and membership

--- resources/views/dashboard/facility/index.blade.php
+++ resources/views/dashboard/facility/index.blade.php
@@ -177,7 +177,7 @@
                         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                         <path d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z" />
                     </svg>
-                    <span class="text-[10px]">${item.rating}</span>
+                    <span class="text-[10px] font-bold">${item.rating}</span>
                 </div>
             </div>`;
         }
@@ -193,7 +193,7 @@
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                 <path d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z" />
                             </svg>
-                            <span class="text-[10px]">${item.rating}</span>
+                            <span class="text-[10px] font-bold">${item.rating}</span>
                         </div>
                     </div>
                 `;
@@ -204,7 +204,7 @@
                 <div class="flex justify-between items-center gap-2 flex-wrap">
                     <h5 class="font-semibold text-xs md:text-base leading-tight">${item.title}</h5>
                     <div class="bg-[#EDEFF1] flex items-center gap-1 rounded-full px-2 py-1 shrink-0">
-                        <span class="text-[8px] leading-tight">Membership 325rb/bln</span>
+                        <span class="text-[8px] leading-tight font-bold">Membership 325rb/bln</span>
                     </div>
                 </div>
             `;
@@ -217,7 +217,7 @@
                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                             <path d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z" />
                         </svg>
-                        <span class="text-[10px]">${item.rating}</span>
+                        <span class="text-[10px] font-bold">${item.rating}</span>
                     </div>
                 `;
             }
